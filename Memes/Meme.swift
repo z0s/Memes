a54aaecb65ed7d1583439fdba5ec493a0447880d
Zeus <masgar001@gmail.com>
2016-09-25 20:15:59 -0700

    feat: Update data model for Memes.

---
+++ Memes/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  Memes
+//
+//  Created by IT on 9/24/16.
+//  Copyright © 2016 z0s. All rights reserved.
+//
+
+import UIKit
+
+struct Meme {
+    
+    var topText: String
+    var bottomText: String
+    var memeImage: UIImage
+    var memedImage: UIImage
+    
+}
