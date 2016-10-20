ce655527e7b3d6956f432b19b97ca76473fafa85
Zeus <masgar001@gmail.com>
2016-10-20 14:18:58 -0700

    feat: Update Detail View Controller

--- Memes/MemesDetailViewController.swift
+++ Memes/MemesDetailViewController.swift
@@ -9,9 +9,30 @@
 import UIKit
 
 class MemesDetailViewController: UIViewController {
+    
+    let detailImageView = UIImageView()
+    
+    var selectedMeme: Meme!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        detailImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        view.addSubview(detailImageView)
+        
+        detailImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        detailImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        detailImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        detailImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        detailImageView.image = selectedMeme.memedImage
+        detailImageView.contentMode = .scaleAspectFit
+        
+        
 
         // Do any additional setup after loading the view.
     }
